ad09d7f62110d5d56d7b6d1c07bcb7640a147f9f
mikluxa <teterevkolya21@gmail.com>
2024-03-22 11:46:29 +0300

    updated struct of client files

---
+++ src/async_client/blocking_tcp_echo_client.h
@@ -0,0 +1,16 @@
+//
+// Created by user on 22.03.24.
+//
+
+#ifndef ASSING_IN_OUT_BLOCKING_TCP_ECHO_CLIENT_H
+#define ASSING_IN_OUT_BLOCKING_TCP_ECHO_CLIENT_H
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+enum { max_length = 1024 };
+
+#endif //ASSING_IN_OUT_BLOCKING_TCP_ECHO_CLIENT_H
--- src/async_client/main.cpp
+++ src/async_client/main.cpp
@@ -1,21 +1,7 @@
 //
-// blocking_tcp_echo_client.cpp
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Created by user on 22.03.24.
 //
-// Copyright (c) 2003-2022 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-
-#include <cstdlib>
-#include <cstring>
-#include <iostream>
-#include <boost/asio.hpp>
-
-using boost::asio::ip::tcp;
-
-enum { max_length = 1024 };
+#include "blocking_tcp_echo_client.h"
 
 int main(int argc, char* argv[])
 {
