3a0c019ec6032fea24cc8571749591f571260848
mikluxa <teterevkolya21@gmail.com>
2024-03-22 13:37:22 +0300

    remote files from gitignore

--- cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/CLionProjects/assing-in-out/test
-# Build directory: /home/user/CLionProjects/assing-in-out/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(executor_test "/home/user/CLionProjects/assing-in-out/cmake-build-debug/test/executor_test")
-set_tests_properties(executor_test PROPERTIES  WORKING_DIRECTORY "/home/user/CLionProjects/assing-in-out/test" _BACKTRACE_TRIPLES "/home/user/CLionProjects/assing-in-out/test/CMakeLists.txt;14;add_test;/home/user/CLionProjects/assing-in-out/test/CMakeLists.txt;0;")
--- src/async_server/main.cpp
+++ src/async_server/main.cpp
@@ -8,9 +8,10 @@ int main(int argc, char* argv[])
     {
         if (argc != 2)
         {
-            std::cerr << "Usage: async_tcp_echo_server <port>\n";
+            std::cerr << "Usage: async_tcp_echo_server <port>\n" << argc << argv[1];
             return 1;
         }
+        std::cerr << "Tests" << argc << std::endl << argv[1] << std::endl;
 
         boost::asio::io_context io_context;
 
