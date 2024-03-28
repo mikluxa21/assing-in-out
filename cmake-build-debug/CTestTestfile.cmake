a3b33c19c0fe08244d58424414508d9d4b978116
mikluxa <teterevkolya21@gmail.com>
2024-03-28 13:50:51 +0300

    Helping commit

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/CLionProjects/assing-in-out
-# Build directory: /home/user/CLionProjects/assing-in-out/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src/async_client")
-subdirs("src/async_server")
-subdirs("test")
