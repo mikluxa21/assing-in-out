3d3147923b95d3fba7a3c9a76929cbdb89b34bff
mikluxa <teterevkolya21@gmail.com>
2024-03-28 13:36:21 +0300

    Created workink version for client? add cikle

--- src/async_client/src/main.cpp
+++ src/async_client/src/main.cpp
@@ -5,37 +5,12 @@
 
 int main(int argc, char* argv[])
 {
-    try
+    if (argc != 3)
     {
-        if (argc != 3)
-        {
-            std::cerr << "Usage: blocking_tcp_echo_client <host> <port>\n";
-            return 1;
-        }
-
-        boost::asio::io_context io_context;
-
-        tcp::socket s(io_context);
-        tcp::resolver resolver(io_context);
-        boost::asio::connect(s, resolver.resolve(argv[1], argv[2]));
-
-        std::cout << "Enter message: ";
-        char request[max_length];
-        std::cin.getline(request, max_length);
-        size_t request_length = std::strlen(request);
-        boost::asio::write(s, boost::asio::buffer(request, request_length));
-
-        char reply[max_length];
-        size_t reply_length = boost::asio::read(s,
-                                                boost::asio::buffer(reply, request_length));
-        std::cout << "Reply is: ";
-        std::cout.write(reply, reply_length);
-        std::cout << "\n";
+        std::cerr << "Usage: blocking_tcp_echo_client <host> <port>\n";
+        return 1;
     }
-    catch (std::exception& e)
-    {
-        std::cerr << "Exception: " << e.what() << "\n";
-    }
-
+    auto* client = new Client();
+    delete client;
     return 0;
 }
