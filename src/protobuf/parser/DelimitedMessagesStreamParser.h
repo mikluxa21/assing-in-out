481abf43c006081eb7ce8b407d1f5cc20f7026f0
mikluxa <teterevkolya21@gmail.com>
2024-03-29 12:10:36 +0300

    Dont work version. Error in the delimitedMessage, method in cpp file

--- src/protobuf/parser/DelimitedMessagesStreamParser.h
+++ src/protobuf/parser/DelimitedMessagesStreamParser.h
@@ -3,38 +3,17 @@
 #include "Parser.h"
 
 
-#ifndef SRC_PROTOBUF_PARSER_DELIMITEDMESSAGESSTREAMPARSER_HPP_
-#define SRC_PROTOBUF_PARSER_DELIMITEDMESSAGESSTREAMPARSER_HPP_
+#ifndef PROTOBUF_PARSER_DELIMITEDMESSAGESSTREAMPARSER_H
+#define PROTOBUF_PARSER_DELIMITEDMESSAGESSTREAMPARSER_H
 
 template<class MessageType>
 class DelimitedMessagesStreamParser
 {
 public:
   typedef std::shared_ptr<const MessageType> PointerToConstValue;
-  std::list<PointerToConstValue> parse(const std::string& data)
-  {
-          std::list<PointerToConstValue> result;
-          this->m_buffer.insert(this->m_buffer.end(), data.begin(), data.end());
-          size_t bytesConsumed = 0;
-          while(!this->m_buffer.empty())
-          {
-            auto msg = parseDelimited<MessageType>(this->m_buffer.data(), 
-        				           this->m_buffer.size(),
-        				           &bytesConsumed
-     					          );
-            if(bytesConsumed > 0)
-            {
-              result.push_back(msg);
-              this->m_buffer.erase(this->m_buffer.begin(),
-        		           this->m_buffer.begin() + bytesConsumed);
-            }
-            else
-              break;
-      }
-    return result;
-  }
+  std::list<PointerToConstValue> parse(const std::string&);
 private:
   std::vector<char> m_buffer = {};
 };
 
-#endif /* SRC_PROTOBUF_PARSER_DELIMITEDMESSAGESSTREAMPARSER_HPP_ */
+#endif /* PROTOBUF_PARSER_DELIMITEDMESSAGESSTREAMPARSER_H */
