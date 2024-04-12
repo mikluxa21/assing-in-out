42d89b94a316af00fdfc5e86a33c109cdb0c88d7
mikluxa <teterevkolya21@gmail.com>
2024-04-12 13:05:02 +0300

    Add tests for test

--- test/Test_server/Test_async_tcp_echo_server.cpp
+++ test/Test_server/Test_async_tcp_echo_server.cpp
@@ -3,10 +3,17 @@
 //
 #include "async_server/async_tcp_echo_server.h"
 
+#include "async_server/async_tcp_echo_server.h"
+
 #include <gtest/gtest.h>
+short port = 8080;
 
-
-TEST(Restest345, Restest)
+TEST(ServerTests, EmptyTest)
 {
-    ASSERT_TRUE(true);
+    boost::asio::io_context io_context;
+
+    server s(io_context, port);
+
+    io_context.run();
+
 }
