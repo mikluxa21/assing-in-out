9b7e4be23324a647341f2802ab3cee301e9d8e2f
mikluxa21 <teterevkolya21@gmail.com>
2024-04-24 13:28:39 +0300

    Add devices library

---
+++ src/devices/DeviceServer.h
@@ -0,0 +1,14 @@
+//
+// Created by mikluxa on 24.04.24.
+//
+
+#ifndef ASYNC_DEVICESERVER_H
+#define ASYNC_DEVICESERVER_H
+
+
+class DeviceServer {
+
+};
+
+
+#endif //ASYNC_DEVICESERVER_H
---
+++ src/devices/src/DeviceServer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mikluxa on 24.04.24.
+//
+
+#include "devices/DeviceServer.h"
