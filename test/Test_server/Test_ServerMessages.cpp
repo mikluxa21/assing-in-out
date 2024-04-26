4eb449fa9d844aa80edb47d4aafe73777140f847
mikluxa <teterevkolya21@gmail.com>
2024-04-26 14:07:46 +0300

    Add not work version Test_ServerMessages.cpp

--- test/Test_server/Test_ServerMessages.cpp
+++ test/Test_server/Test_ServerMessages.cpp
@@ -6,13 +6,90 @@
 
 #include <gtest/gtest.h>
 
-TEST(ServerMessages, GetIdTest)
+namespace
 {
-    int digit = 0;
+    std::string captureOutput(std::function<void()> func)
+    {
+        std::stringstream buffer;
+        std::streambuf *old = std::cout.rdbuf(buffer.rdbuf());
+        func();
+        std::cout.rdbuf(old);
+        return buffer.str();
+    }
+}
+
+TEST(ServerMessages, GetIdOneTest)
+{
+    int digit = 1;
     ServerMessages serverMessages;
     InterfaceProtobufMessage interfaceProtobufMessage;
     auto getID = interfaceProtobufMessage.CreateMessage("GetId");
     auto serverAnsuer = serverMessages.GetServerQueshion(getID);
-    auto rightMessage = interfaceProtobufMessage.CreateMessage(digit);
+    InterfaceProtobufMessage interfaceProtobufMessage2;
+    auto rightMessage = interfaceProtobufMessage2.CreateMessage(digit);
     ASSERT_EQ(serverAnsuer, rightMessage);
 }
+
+TEST(ServerMessages, GetIdAnyTest)
+{
+    int count= 10;
+    std::vector<std::string> vecServerAnswer = {};
+    ServerMessages serverMessages;
+    InterfaceProtobufMessage interfaceProtobufMessage;
+    auto getID = interfaceProtobufMessage.CreateMessage("GetId");
+    for(size_t i = 0; i < count; i++)
+        vecServerAnswer.push_back(serverMessages.GetServerQueshion(getID));
+    for(size_t i = 0; i < count; i++)
+    {
+        InterfaceProtobufMessage interfaceProtobufMessage2;
+        auto rightMessage = interfaceProtobufMessage2.CreateMessage((i+1));
+        ASSERT_EQ(vecServerAnswer[i], rightMessage);
+    }
+}
+
+TEST(ServerMessages, FaultTest)
+{
+    ServerMessages serverMessages;
+    std::string wrongData = "\x05wrong";
+    ASSERT_THROW(serverMessages.GetServerQueshion(wrongData), std::runtime_error);
+}
+
+TEST(ServerMessages, StreamOneConectedTest)
+{
+    ServerMessages serverMessages;
+    InterfaceProtobufMessage interfaceProtobufMessage;
+    auto getID = interfaceProtobufMessage.CreateMessage("GetId");
+    std::string stream = captureOutput([&]()
+            {serverMessages.GetServerQueshion(getID);}
+            );
+    EXPECT_TRUE(stream.find("connection established: 1") != std::string::npos);
+}
+
+TEST(ServerMessages, StreamAnyConectedTest)
+{
+    int count= 10;
+    ServerMessages serverMessages;
+    InterfaceProtobufMessage interfaceProtobufMessage;
+    auto getID = interfaceProtobufMessage.CreateMessage("GetId");
+    std::string stream = captureOutput([&]()
+                                       {
+                                           for(size_t i = 0; i < count; i++)
+                                               serverMessages.GetServerQueshion(getID);
+                                       }
+    );
+    for(size_t i = 0; i < count; i++)
+    {
+        std::string message = "connection established: " + std::to_string(i + 1);
+        EXPECT_TRUE(stream.find(message) != std::string::npos);
+    }
+}
+
+/*TEST(ServerMessages, ReturnedOneMessageTest)
+{
+    int digit = 1;
+    ServerMessages serverMessages;
+    InterfaceProtobufMessage interfaceProtobufMessage;
+    auto getID = interfaceProtobufMessage.CreateMessage("GetId");
+    auto serverAnsuer = serverMessages.GetServerQueshion(getID);
+    auto data
+}*/
