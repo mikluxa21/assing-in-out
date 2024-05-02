67cdea57961cf4c7b5e31c7b7a3a9e59bd3f651c
mikluxa <teterevkolya21@gmail.com>
2024-05-02 12:43:08 +0300

    Added empty parallel executor

---
+++ src/parallel_executor/ParallelclientExecutor.h
@@ -0,0 +1,14 @@
+//
+// Created by user on 02.05.24.
+//
+
+#ifndef ASYNC_PARALLELCLIENTEXECUTOR_H
+#define ASYNC_PARALLELCLIENTEXECUTOR_H
+
+
+class ParallelclientExecutor {
+
+};
+
+
+#endif //ASYNC_PARALLELCLIENTEXECUTOR_H
---
+++ src/parallel_executor/src/ParallelclientExecutor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by user on 02.05.24.
+//
+
+#include "parallel_executor/ParallelclientExecutor.h"
