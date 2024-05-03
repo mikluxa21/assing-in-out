70736e5f7e9384f677283ef3e65c3541d54debb8
mikluxa <teterevkolya21@gmail.com>
2024-05-03 12:57:29 +0300

    Created main for the test

--- src/parallel_executor/src/main.cpp
+++ src/parallel_executor/src/main.cpp
@@ -1,3 +1,11 @@
 //
 // Created by user on 02.05.24.
 //
+#include "parallel_executor/ParallelClientExecutor.h"
+
+int main()
+{
+    ParallelClientExecutor parallelClientExecutor;
+    parallelClientExecutor.ParallWorker(6, 1);
+    return 0;
+}
